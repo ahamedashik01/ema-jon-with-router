b560d0d005dbb078088fbb76d20b71d787aa8903
ahamedashik01 <ashikshamim.007@gmail.com>
2021-10-02 01:55:17 +0600

    useCart function hook added

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const OrderReview = () => {
     const [products] = useProducts();
+    const [cart] = useCart(products);
+    console.log(cart)
     return (
         <div>
             <h1>{products.length}</h1>
             <h2>This is Order Review</h2>
+
+            <h3>{cart.length}</h3>
         </div>
     );
 };
